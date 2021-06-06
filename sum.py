474fe30e31e506d3f4c8b9824d00a76931491816
L-O-K-E-S-H-P <84820086+L-O-K-E-S-H-P@users.noreply.github.com>
2021-06-06 11:39:12 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,3 +1,7 @@
+''' Create a function to compute sum of digits. Call this sum of digits function 
+to find the sum of digits of numbers ranges from 1001 to 22000'''
+
+
 def sum_of_digits(num):
     num=str(num)
     sum=0
